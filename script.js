25f51dba608175a2adec56f40a66be5809929c51
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-25 20:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // your code here
+
+document.getElementById("urlForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent page refresh
+
+let name = document.getElementById("name").value.trim();
+let year = document.getElementById("year").value.trim();
+let baseurl ="https://localhost:8080/";
+	
+let params=[];
+	 if(name){
+		 params.push(`name=${name}`);
+	 }
+
+	if(year){
+		params.push('year=${year}');
+	}
+
+	if(params.length>0){
+		baseurl += "?" + params.join("&");
+	}
+	document.getElementById("url").textContent = baseurl;
+	
+});
+
+
